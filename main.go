7811e8e57725e5668168912cb6681ab2384f068b
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-24 22:46:26 -0300

    Define first structs

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+}
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
