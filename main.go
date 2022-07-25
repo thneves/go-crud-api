a5dd82ad58f102f17cb1045fbe6eae3823d02f95
thales-pinner <thales.neves@pinpeople.com.br>
2022-07-24 22:52:28 -0300

    add crud routes

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-}
+	"github.com/gorilla/mux"
+)
 
 type Movie struct {
 	ID       string    `json:"id"`
@@ -14,4 +18,19 @@ type Movie struct {
 type Director struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/movies", getMovies).Methods("GET")
+	route.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	route.HandleFunc("/movies", createMovie).Methods("POST")
+	route.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	route.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Server started at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", route))
 }
